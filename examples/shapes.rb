7a61c7e61162db5a4f90b025f887a871492defda
James Healy <jimmy@deefa.com>
2008-05-22 15:23:11 +1000

    added to .gitignore and tweaked example

--- examples/shapes.rb
+++ examples/shapes.rb
@@ -7,7 +7,7 @@ require 'pdf/wrapper'
 
 pdf = PDF::Wrapper.new(:paper => :A4)
 pdf.rectangle(30,30,100,100, :fill_color => :red)
-pdf.circle(100,300,30, :fill_color => :red)
+pdf.circle(100,300,30)
 pdf.line(100, 350, 400, 150)
 pdf.rectangle(300,300, 200, 200, :fill_color => :green, :radius => 10)
 pdf.render_to_file("shapes.pdf")
