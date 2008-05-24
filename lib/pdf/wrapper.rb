f5b91f04e5070a7d0968c5f5f9d9228c7ab62cbb
James Healy <jimmy@deefa.com>
2008-05-24 20:15:58 +1000

    move a few more functions out into a seperate file

--- lib/pdf/wrapper.rb
+++ lib/pdf/wrapper.rb
@@ -5,6 +5,7 @@ require 'pdf/core'
 
 require File.dirname(__FILE__) + "/wrapper/graphics"
 require File.dirname(__FILE__) + "/wrapper/images"
+require File.dirname(__FILE__) + "/wrapper/loading"
 require File.dirname(__FILE__) + "/wrapper/table"
 require File.dirname(__FILE__) + "/wrapper/text"
 
@@ -465,53 +466,6 @@ module PDF
         :width  => points_to_right_margin(x),
         :height => points_to_bottom_margin(y)
       }
-    end
-
-    # load libpango if it isn't already loaded.
-    # This will add some methods to the cairo Context class in addition to providing
-    # its own classes and constants. A small amount of documentation is available at
-    # http://ruby-gnome2.sourceforge.jp/fr/hiki.cgi?Cairo%3A%3AContext#Pango+related+APIs
-    def load_libpango
-      begin
-        require 'pango' unless @context.respond_to? :create_pango_layout
-      rescue LoadError
-        raise LoadError, 'Ruby/Pango library not found. Visit http://ruby-gnome2.sourceforge.jp/'
-      end
-    end
-
-    # load lib gdkpixbuf if it isn't already loaded.
-    # This will add some methods to the cairo Context class in addition to providing
-    # its own classes and constants.
-    def load_libpixbuf
-      begin
-        require 'gdk_pixbuf2' unless @context.respond_to? :set_source_pixbuf
-      rescue LoadError
-        raise LoadError, 'Ruby/GdkPixbuf library not found. Visit http://ruby-gnome2.sourceforge.jp/'
-      end
-    end
-
-    # load lib poppler if it isn't already loaded.
-    # This will add some methods to the cairo Context class in addition to providing
-    # its own classes and constants.
-    def load_libpoppler
-      begin
-        require 'poppler' unless @context.respond_to? :render_poppler_page
-      rescue LoadError
-        raise LoadError, 'Ruby/Poppler library not found. Visit http://ruby-gnome2.sourceforge.jp/'
-      end
-    end
-
-    # load librsvg if it isn't already loaded
-    # This will add an additional method to the Cairo::Context class
-    # that allows an existing SVG to be drawn directly onto it
-    # There's a *little* bit of documentation at:
-    # http://ruby-gnome2.sourceforge.jp/fr/hiki.cgi?Cairo%3A%3AContext#render_rsvg_handle
-    def load_librsvg
-      begin
-        require 'rsvg2' unless @context.respond_to? :render_svg_handle
-      rescue LoadError
-        raise LoadError, 'Ruby/RSVG library not found. Visit http://ruby-gnome2.sourceforge.jp/'
-      end
     end
 
     # save and restore the cursor position around a block
